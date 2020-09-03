2c01ae07465687163b16717b7a70b0ed0de06696
Alex Pozas <apozas@users.noreply.github.com>
2020-09-03 16:41:24 +0200

    Add forgotten disclaimer

--- android_app/app/src/main/java/com/apozas/contactdiary/ContactDatabase.kt
+++ android_app/app/src/main/java/com/apozas/contactdiary/ContactDatabase.kt
@@ -1,5 +1,20 @@
 package com.apozas.contactdiary
 
+/*
+    This file is part of Contact Diary.
+    Contact Diary is free software: you can redistribute it and/or modify
+    it under the terms of the GNU General Public License as published by
+    the Free Software Foundation, either version 3 of the License, or
+    (at your option) any later version.
+    Contact Diary is distributed in the hope that it will be useful,
+    but WITHOUT ANY WARRANTY; without even the implied warranty of
+    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+    GNU General Public License for more details.
+    You should have received a copy of the GNU General Public License
+    along with Contact Diary. If not, see <http://www.gnu.org/licenses/>.
+    Copyright 2020 by Alex Pozas-Kerstjens (apozas)
+*/
+
 import android.provider.BaseColumns
 
 class ContactDatabase {
