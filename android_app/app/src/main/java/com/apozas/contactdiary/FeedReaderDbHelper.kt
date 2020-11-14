9fd96c37e4815dceed80a4b84ea1980e59050021
Alex Pozas <apozas@users.noreply.github.com>
2020-11-14 10:45:31 +0100

    New list ordering
    Now it orders by date, and alphabetically for same date

--- android_app/app/src/main/java/com/apozas/contactdiary/FeedReaderDbHelper.kt
+++ android_app/app/src/main/java/com/apozas/contactdiary/FeedReaderDbHelper.kt
@@ -42,10 +42,12 @@ class FeedReaderDbHelper(context: Context) : SQLiteOpenHelper(context, DATABASE_
         val query = if (onlyRisky) {
             "Select * from " + ContactDatabase.ContactDatabase.FeedEntry.TABLE_NAME +
                     " WHERE " + ContactDatabase.ContactDatabase.FeedEntry.CLOSECONTACT_COLUMN + ">1" +
-                    " ORDER BY " + ContactDatabase.ContactDatabase.FeedEntry.DATETIME_COLUMN + " DESC"
+                    " ORDER BY " + ContactDatabase.ContactDatabase.FeedEntry.DATETIME_COLUMN + " DESC," +
+                    " " + ContactDatabase.ContactDatabase.FeedEntry.NAME_COLUMN + " ASC"
         } else {
             "Select * from " + ContactDatabase.ContactDatabase.FeedEntry.TABLE_NAME +
-                    " ORDER BY " + ContactDatabase.ContactDatabase.FeedEntry.DATETIME_COLUMN + " DESC"
+                    " ORDER BY " + ContactDatabase.ContactDatabase.FeedEntry.DATETIME_COLUMN + " DESC," +
+                    " " + ContactDatabase.ContactDatabase.FeedEntry.NAME_COLUMN + " ASC"
         }
 
         return db.rawQuery(query, null)
